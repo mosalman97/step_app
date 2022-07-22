72ec5e558eb76941a90a28fd70f81eb8be01a339
mosalman97 <mosalman1098@gmail.com>
2022-07-22 20:44:22 +0530

    responsive screen package added

---
+++ src/components/screens/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+
+const Welcome = ()=>{
+    return (
+       <SafeAreaView>
+          
+       </SafeAreaView>
+    )
+}
+
+export default Welcome
